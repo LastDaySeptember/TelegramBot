522334f5bdde28bb08704819c2c6ef93d19f667c
LastDaySeptember <alia.stikova@gmail.com>
2024-12-01 19:16:55 +0100

    ADD conversionHandler

--- bot.py
+++ bot.py
@@ -2,8 +2,8 @@ import os
 from dotenv import load_dotenv  # Импортируем dotenv для работы с .env файлами
 from utils import gpt, util
 import datetime
-from telegram import Update, ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes, MessageHandler, filters, CallbackQueryHandler
+from telegram import Update, InlineKeyboardMarkup, InlineKeyboardButton
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes, MessageHandler, filters, CallbackQueryHandler, ConversationHandler
 
 # import tokens
 load_dotenv()
@@ -11,10 +11,14 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 GPT_TOKEN = os.getenv("ChatGPT_TOKEN")
 chat_gpt = gpt.ChatGptService(GPT_TOKEN)
 
+
+# states
+TALK_GPT, TALK_PEOPLE, QUIZ, IMAGE_DESCRIPTION, SCENE_FIND = range(5)
+
 # functions
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await util.send_image(update, context, "bot")
-    text = "You can :\n/fact - get random fact,\n/quiz - get and answer quiz question,\n/talk - talk with interesting characters,\n/gpt - talk with gpt,\n/image - send image to describe it by AI,\n/scene - ask GPT to find scene from movie, close to your description."
+    text = "You can:\n/fact - get random fact,\n/quiz - get and answer quiz question,\n/talk - talk with interesting characters,\n/gpt - talk with gpt,\n/image - send image to describe it by AI,\n/scene - ask GPT to find scene from movie, close to your description."
     if update.message:
         await update.message.reply_text(
             f"Hello, {update.effective_user.first_name}.Welcome to the telegram bot! {text}")
@@ -43,24 +47,28 @@ async def give_random_fact(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 
 
-async def give_quiz():
-    pass
+async def give_quiz(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await util.send_image(update, context, "quiz")
 
 
-async def talk_people():
-    pass
+async def talk_people(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await util.send_image(update,context, "talk")
 
 
-async def talk_gpt():
-    pass
+async def talk_gpt(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await util.send_image(update, context, "gpt")
+    message = await util.send_text(update, context, "Let's talk.")
 
 
-async def describe_image():
-    pass
+async def describe_image(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await util.send_image(update, context, "eye")
+    await util.send_text(update, context, "Please, send picture to get the description")
+    return IMAGE_DESCRIPTION
 
 
-async def find_scene():
-    pass
+async def find_scene(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await util.send_image(update, context, "film")
+    await util.send_text(update, context, "Describe the scene you want to see:")
 
 
 async def fact_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -72,21 +80,66 @@ async def fact_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
     else:
         await start(update, context)
 
+
+async def image_description(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    pass
+
 def main():
     app = ApplicationBuilder().token(BOT_TOKEN).build()
+    conv_handler_talk = ConversationHandler(
+        entry_points = [CommandHandler("talk", talk_people)],
+        states={
+            TALK_PEOPLE:[CommandHandler("talk", talk_people)],
+        },
+        fallbacks=[CommandHandler("start", start)],
+    )
+
+    conv_handler_gpt = ConversationHandler(
+        entry_points = [CommandHandler("gpt", talk_gpt)],
+        states={
+            TALK_GPT:[CommandHandler("gpt", talk_gpt)],
+        },
+        fallbacks=[CommandHandler("start", start)],
+    )
+
+    conv_handler_quiz = ConversationHandler(
+        entry_points = [CommandHandler("quiz", give_quiz)],
+        states={
+            QUIZ:[CommandHandler("quiz", give_quiz)],
+        },
+        fallbacks=[CommandHandler("start", start)],
+    )
+
+    conv_handler_image = ConversationHandler(
+        entry_points = [CommandHandler("image", describe_image)],
+        states={
+            IMAGE_DESCRIPTION:[CommandHandler("image", describe_image)],
+        },
+        fallbacks=[CommandHandler("start", start)],
+    )
+
+
+    conv_handler_scene= ConversationHandler(
+        entry_points = [CommandHandler("scene", find_scene)],
+        states={
+            SCENE_FIND:[CommandHandler("scene", find_scene)],
+        },
+        fallbacks=[CommandHandler("start", start)],
+    )
+
     app.add_handler(CommandHandler("start", start))
-    app.add_handler(CommandHandler("fact", give_random_fact))
-    app.add_handler(CommandHandler("quiz", give_quiz))
-    app.add_handler(CommandHandler("talk", talk_people))
-    app.add_handler(CommandHandler("gpt", talk_gpt))
-    app.add_handler(CommandHandler("image", describe_image))
-    app.add_handler(CommandHandler("scene", find_scene))
 
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
+    app.add_handler(MessageHandler(filters.PHOTO, image_description))
 
     app.add_handler(CallbackQueryHandler(fact_handler, '^fact_.*'))
 
     # run app
+    app.add_handler(conv_handler_talk)
+    app.add_handler(conv_handler_gpt)
+    app.add_handler(conv_handler_quiz)
+    app.add_handler(conv_handler_image)
+    app.add_handler(conv_handler_scene)
     app.run_polling()
 
 
